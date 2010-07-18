efba5451a6add75f5b03a5dd8a8e6ad2cd2490b5
Brendan Hickey <brendan@bhickey.net>
2010-07-18 18:45:38 -0400

    Added a project file with penumbra dependency

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject mapatorious "0.0.0"
+  :description "A proceudral generation project"
+  :dependencies [[penumbra "0.6.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.2.0-master-SNAPSHOT"] 
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[native-deps "1.0.1"]
+		     [autodoc "0.7.1"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]
+                     [leiningen/lein-swank "1.1.0"]])
