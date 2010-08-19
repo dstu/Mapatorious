5ccc1052924fa3d2a55a301314f059fc479b45e0
Brendan Hickey <brendan@bhickey.net>
2010-08-19 00:04:59 -0400

    Simplified version of the selection algorithm

--- src/main/clojure/util/select.clj
+++ src/main/clojure/util/select.clj
@@ -8,20 +8,6 @@
           mid (bit-shift-right cnt 1)]
       (nth ns mid))))
 
-(defn trisect [f n]
-  (loop [ns n
-        [a b c] [[] [] []]]
-    (if (empty? ns)
-      [a b c]
-      (recur
-        (next ns)
-        (let [x (first ns)]
-          (if (> f x)
-            [(conj a x) b c]
-            (if (< f x)
-              [a b (conj c x)]
-              [a (conj b x) c])))))))
-       
 (defn bisect [f n]
   (loop [ns n
         [a b] [[] []]]
@@ -34,19 +20,15 @@
             [(conj a x) b]
             [a (conj b x)]))))))
 
-
 (defn kth-by [l k f]
   (if (< (count l) 10)
     (nth (sort l) k)
     (let [meds    (map (median-slow f) (partition-all 5 l))
           M       (kth-by meds (bit-shift-right (count meds) 1) f)
-          [L1 L2 L3] (trisect M l)
-          csum    (+ (count L1) (count L2))]
+          [L1 L2] (bisect M l)]
         (if (< k (count L1))
             (kth-by L1 k f)
-            (if (> k csum)
-            (kth-by L3 (- k csum) f)
-              M)))))
+            (kth-by L2 (- k (count L1)) f)))))
 
 (defn kth [l k]
   (kth-by l k <))
