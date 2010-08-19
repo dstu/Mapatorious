38fac57deefe23f738bfeedd37cb852138479906
Stu Black <trurl@freeshell.org>
2010-08-18 22:28:00 -0400

    Pushing some hello, world! stuff.
    * src/main/clojure/mapatorius.clj: deleted old junk file
    * src/mapatorius/helloworld.clj: added hello, world file for penumbra/opengl

---
+++ src/mapatorius/helloworld.clj
@@ -0,0 +1,27 @@
+(ns mapatorius.helloworld
+  (:use [penumbra opengl])
+  (:require [penumbra.app :as app]
+	    [penumbra.text :as text]))
+
+;; Dimension of window
+(def *dim* (vec2 800 600))
+
+;; Used as callback
+(defn init [state]
+  "Set up game state once, at beginning of execution."
+  (app/title! "Hello, world!"))
+
+;; Used as callback. Has two args, which we ignore.
+(defn display [_ _]
+  (text/with-font (text/font "Inconsolata" :size 12)
+    (text/write-to-screen "Hello, world!" 30 20)
+    (app/repaint!)))
+
+;; Call (mapatorius.helloworld/start) to start the display. This
+;; calls penumbra.app/start with two arguments specifying
+;; certain callbacks and parameters.
+(defn start []
+  "Start the hello world app."
+  (app/start
+   {:init init, :display display}
+   {:dim *dim*}))
