06d33448fa30e415e831ff74c66b504fbacbf5e8
Stu Black <trurl@freeshell.org>
2010-08-18 22:10:37 -0400

    Switched over to leiningen for building and running.
    * .gitignore: added entries for dependencies and some other files auto-generated by leiningen
    * project.clj: modified leiningen build file to pull in lwjgl and penumbra deps properly
    * pom.xml: deleted now-obsolete never-was-working-really Maven build file

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject mapatorious "0.0.0"
+(defproject mapatorius "0.0.0"
   :description "A proceudral generation project"
   :dependencies [[penumbra "0.6.0-SNAPSHOT"]
                  [org.clojure/clojure "1.2.0-master-SNAPSHOT"] 
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :native-dependencies [[penumbra/lwjgl "2.4.2"]]
   :dev-dependencies [[native-deps "1.0.1"]
 		     [autodoc "0.7.1"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
